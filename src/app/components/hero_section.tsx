a6a280a4a2caa5f949ca8825c6537ee4cffb285e
Samuel-chibueze <142575213+Samuel-chibueze@users.noreply.github.com>
2024-07-10 13:22:56 +0100

    latest commit 7/10/2024 1:22pm

--- src/app/components/hero_section.tsx
+++ src/app/components/hero_section.tsx
@@ -1,38 +1,40 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
+import Image1 from '../images/signup-cover.jpeg'
 
-    export default function hero_section(){
-        return(<>
-        <div className="bg-blue-400 h-[650px] flex flex-col items-center justify-center gap-10 px-10 w-full">
-            <div className="w-70px">
-            <h1 className="text-5xl w-30">
-            Upgrade the way
-            you trade
-            </h1>
-        </div>
-         <div>
-            <p>Trade with the world’s largest retail broker and benefit from better-than-market conditions.</p>
-         </div>
-        
-       
-       <div className="flex space-x-4">
+export default function hero_section() {
+  return (<>
+    <div className="bg-blue-400 h-[650px] flex flex-col items-center justify-center gap-10 px-10 w-full bg-cover" style={{ backgroundImage: `url(${Image1.src})` }}>
+      <div className="w-70px">
+        <h1 className="text-5xl w-30">
+          Upgrade the way
+          you trade
+        </h1>
+      </div>
+      <div>
+        <p>Trade with the world’s largest retail broker and benefit from better-than-market conditions.</p>
+      </div>
+
+
+      <div className="flex space-x-4">
         <Link href="/signin" className="text-gray-700 font-4 hover:text-gray-900  px-7 rounded-md py-3 bg-yellow-300 text-lg font-medium">
           Sign In
         </Link>
         <Link href="/signin" className="text-gray-700 hover:text-gray-900  px-5  text-lg font-medium rounded-md py-3 bg-gray-200 ">
           Sign Up
         </Link>
-      </div> 
-        </div>
+      </div>
+    </div>
 
-        <div className="flex justify-center items-center text-lg gap-4 font-bold italic px-10 py-10 bt-20 border border-b-gray-300">
-            <p>800,000+ active traders</p>
-            <p>Multiple regulatory licenses</p>
-            <p> customer support</p>
-            <p>PCI DSS certified</p>
-        </div>
+    <div className="flex flex-col md:flex-row justify-center items-center text-lg gap-4 md:gap-8 font-bold  px-10 py-10 bt-20 border border-b-gray-300 ">
+      <p>800,000+ active traders</p>
+      <p>Multiple regulatory licenses</p>
+      <p> customer support</p>
+      <p>PCI DSS certified</p>
+    </div>
 
-        </>);
-    }
+  </>);
+}
 
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,20 +23,19 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} bg-gray-100 text-gray-900`}>
         <Navbar />
-        <NextTopLoader 
-         color="#2299DD"
-         initialPosition={0.08}
-         crawlSpeed={200}
-         height={3}
-         crawl={true}
-         showSpinner={true}
-         easing="ease"
-         speed={200}
-         shadow="0 0 10px #2299DD,0 0 5px #2299DD"
-         template='<div class="bar" role="bar"><div class="peg"></div></div> 
-         <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
-         zIndex={1600}
-         showAtBottom={false}/>
+        <NextTopLoader
+  color="#2299DD"
+  initialPosition={0.08}
+  crawlSpeed={200}
+  height={3}
+  crawl={true}
+  easing="ease"
+  speed={200}
+  shadow="0 0 10px #2299DD, 0 0 5px #2299DD"
+  template='<div class="bar" role="bar"><div class="peg"></div></div>'
+  zIndex={1600}
+  showAtBottom={false}
+/>
         {children}
         <Footer />
       </body>
